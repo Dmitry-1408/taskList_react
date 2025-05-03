faf77544d1404ba68bdf427efb39618dc67b85da
Dmitry Bykov <bykovdmnic@outlook.com>
2025-05-03 17:26:33 +0300

    Условный рендеринг для открытия и закрытия окон

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,61 @@
+import { useState } from "react";
+
 function App() {
+  /* useState на закрытие крестиком */
+  const [openSection, setOpenSection] = useState({
+    tasklist: false,
+    task: true,
+    complitedTasks: true,
+  });
+
+  /* универсальная функция открытия/закрытия */
+  function toggleSection(section) {
+    setOpenSection((prev) => ({ ...prev, [section]: !prev[section] }));
+  }
+
+  /* логика работы приложения */
   return (
     <div className="app">
       {/* 1 task */}
       <div className="task-container">
         <h1>Task List with Priority</h1>
-        <button className="close-button">+</button>
-        <TaskForm />
+        <button
+          className={`close-button ${openSection.tasklist ? "open" : ""}`}
+          onClick={() => toggleSection("tasklist")}
+        >
+          +
+        </button>
+        {/* при нажатии на крестик открытие/закрытие */}
+        {openSection.tasklist && <TaskForm />}
       </div>
 
       {/* 2 task */}
       <div className="task-container">
         <h2>Tasks:</h2>
-        <button className="close-button">+</button>
+        <button
+          className={`close-button ${openSection.task ? "open" : ""}`}
+          onClick={() => toggleSection("task")}
+        >
+          +
+        </button>
         <div className="sort-controls">
           <button className="sort-button">By Date</button>
           <button className="sort-button">By Priority</button>
         </div>
-        <TaskList />
+        {openSection.task && <TaskList />}
       </div>
 
       {/* 3 task */}
       <div className="completed-task-container">
         <h2>Completed Tasks</h2>
-        <button className="close-button">+</button>
-        <CompletedTaskList />
+        <button
+          className={`close-button ${openSection.complitedTasks ? "open" : ""}`}
+          onClick={() => toggleSection("complitedTasks")}
+        >
+          +
+        </button>
+        {openSection.complitedTasks && <CompletedTaskList />}
+        
       </div>
 
       {/* footer */}
